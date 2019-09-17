58e1577294fff082f6d5702a37c311139f902162
neumaier <sebastian.neumaier@wu.ac.at>
2019-09-17 12:07:29 +0200

    new evolution plots

--- docker/ui.sh
+++ docker/ui.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --rm --network="host" odpw -v ODPWUI 1>> /home/opwu/portalwatch_data/odpw_ui.out 2> /home/opwu/portalwatch_data/odpw_ui.err
+docker run --rm -d --name odpwui --network="host" odpw -v ODPWUI 1>> /home/opwu/portalwatch_data/odpw_ui.out 2> /home/opwu/portalwatch_data/odpw_ui.err
